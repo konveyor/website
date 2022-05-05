cc683bf9100bc6c7524d4867d25dadc91957776f
Robert Bottomley <rbottomley@gmail.com>
2022-05-04 17:23:56 -0700

    Fix og:image for report page

--- src/components/seo.js
+++ src/components/seo.js
@@ -31,6 +31,55 @@ function Seo({ description, lang, meta, image, title, article }) {
   const metaDescription = description || site.siteMetadata.description
   const metaImage = getSrc(image) || site.siteMetadata.image
   const defaultTitle = site.siteMetadata?.title
+  const defaultMeta = [
+    {
+      name: `description`,
+      content: `${metaImage} metaDescription`,
+    },
+    {
+      property: `og:title`,
+      content: title ? `${title} | ${defaultTitle}` : defaultTitle,
+    },
+    {
+      property: `og:description`,
+      content: metaDescription,
+    },
+    {
+      property: `og:type`,
+      content: article ? `article` : `website`,
+    },
+    {
+      name: `twitter:card`,
+      content: `summary`,
+    },
+    {
+      name: `twitter:creator`,
+      content: site.siteMetadata?.twitterCreator || ``,
+    },
+    {
+      name: `twitter:title`,
+      content: title ? `${title} | ${defaultTitle}` : defaultTitle,
+    },
+    {
+      name: `twitter:description`,
+      content: metaDescription,
+    },
+  ]
+  const defaultOgImage = [
+    {
+      property: `og:image`,
+      content: `${siteUrl}${metaImage}`,
+    },
+  ]
+
+  // For some reason og:image in meta is being ignored. Only add the default if
+  // meta does not contain it.
+  const found = meta.find(({ property }) => property === 'og:image')
+  let metas = [...meta, ...defaultMeta]
+
+  if (typeof found !== 'object') {
+    metas = metas.concat(defaultOgImage)
+  }
 
   return (
     <Helmet
@@ -41,44 +90,7 @@ function Seo({ description, lang, meta, image, title, article }) {
       title={title}
       titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       defaultTitle={defaultTitle}
-      meta={[
-        {
-          name: `description`,
-          content: `${metaImage} metaDescription`,
-        },
-        {
-          property: `og:title`,
-          content: title ? `${title} | ${defaultTitle}` : defaultTitle,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:image`,
-          content: `${siteUrl}${metaImage}`,
-        },
-        {
-          property: `og:type`,
-          content: article ? `article` : `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.twitterCreator || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title ? `${title} | ${defaultTitle}` : defaultTitle,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
+      meta={metas}
     />
   )
 }
--- src/templates/page.js
+++ src/templates/page.js
@@ -14,8 +14,9 @@ export default function Page({ children, ...props }) {
   const bannerPath = `${props.path}${banner}`.slice(1)
   let bannerImg
 
-  // FIXME: There is a horrible hack in the Seo element. Banner images sent to Seo from blog posts work, but not when
-  // sent from an mdx page. Why? Unknown. Instead share_image points to a static image.
+  // FIXME: There is a horrible hack in the Seo element. Banner images sent to Seo
+  // from blog posts work, but not when sent from an mdx page. Why? Unknown.
+  // Instead share_image points to a static image.
 
   return (
     <StaticQuery
@@ -52,46 +53,43 @@ export default function Page({ children, ...props }) {
         }
       `}
       render={(data) => (
-        <>
+        <div className="page-wrapper !max-w-screen-lg">
           <Seo
             title={title}
             description={description}
             meta={[
               {
-                name: `og:image`,
+                property: `og:image`,
                 content: `https://www.konveyor.io${share_image}`,
               },
             ]}
           />
-          <div className="page-wrapper !max-w-screen-lg">
-            {(bannerImg = data.allFile.nodes.find((n) => n.relativePath === bannerPath)
-              ?.childImageSharp?.gatsbyImageData) && <></>}
-            <Seo title={title} description={description} image={bannerImg} />
 
-            <div>
-              <GatsbyImage
-                className="mt-6 rounded-md before:text-transparent before:absolute before:pointer-events-none before:pointer-events-none before:z-[1] before:w-full before:h-full before:shadow-image before:rounded-md relative rounded-md"
-                image={bannerImg}
-                alt=""
-              />
-            </div>
-            <h1 className="mt-6 font-medium text-3xl md:text-4xl font-headings !leading-snug">
-              <WidowControl text={title} />
-            </h1>
-            <div className="border-b border-base-300 mb-4 py-4 text-base-500">
-              <ShareButtons title={title} url={url} twitterHandle="konveyor_io" />
-            </div>
-            <div className="flex flex-col md:flex-row md:gap-4">{children}</div>
-            <h2 className="mt-8 md:mt-16 font-medium text-3xl md:text-4xl font-headings !leading-snug">
-              <WidowControl text="What to read next" />
-            </h2>
-            <div className="mt-6 mb-8 md:mb-16 grid gird-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {data.allMdx.nodes.map((post) => (
-                <PostCard key={post.id} post={post} condensed="true" />
-              ))}
-            </div>
+          {(bannerImg = data.allFile.nodes.find((n) => n.relativePath === bannerPath)
+            ?.childImageSharp?.gatsbyImageData) && <></>}
+          <div>
+            <GatsbyImage
+              className="mt-6 rounded-md before:text-transparent before:absolute before:pointer-events-none before:pointer-events-none before:z-[1] before:w-full before:h-full before:shadow-image before:rounded-md relative rounded-md"
+              image={bannerImg}
+              alt=""
+            />
           </div>
-        </>
+          <h1 className="mt-6 font-medium text-3xl md:text-4xl font-headings !leading-snug">
+            <WidowControl text={title} />
+          </h1>
+          <div className="border-b border-base-300 mb-4 py-4 text-base-500">
+            <ShareButtons title={title} url={url} twitterHandle="konveyor_io" />
+          </div>
+          <div className="flex flex-col md:flex-row md:gap-4">{children}</div>
+          <h2 className="mt-8 md:mt-16 font-medium text-3xl md:text-4xl font-headings !leading-snug">
+            <WidowControl text="What to read next" />
+          </h2>
+          <div className="mt-6 mb-8 md:mb-16 grid gird-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {data.allMdx.nodes.map((post) => (
+              <PostCard key={post.id} post={post} condensed="true" />
+            ))}
+          </div>
+        </div>
       )}
     />
   )
