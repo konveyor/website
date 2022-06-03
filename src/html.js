530cb9c6383ea31123dba385361abf9e2415f791
Robert Bottomley <rbottomley@gmail.com>
2022-06-03 16:23:43 -0700

    Update CSP for MailChimp

--- src/html.js
+++ src/html.js
@@ -9,7 +9,7 @@ export default function HTML(props) {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta
           httpEquiv="Content-Security-Policy"
-          content="default-src 'self' *; script-src 'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com www.googletagmanager.com chimpstatic.com *.list-manage.com; font-src 'self' fonts.gstatic.com data:; img-src www.googletagmanager.com ssl.gstatic.com www.gstatic.com data: *; style-src 'self' 'unsafe-inline' fonts.googleapis.comi tagmanager.google.com; frame-src 'self' www.slideshare.net www.youtube.com www.googletagmanager.com"
+          content="default-src 'self' *; script-src 'self' 'unsafe-inline' 'unsafe-eval' www.google-analytics.com www.googletagmanager.com chimpstatic.com *.mailchimp.com *.list-manage.com; font-src 'self' fonts.gstatic.com data:; img-src www.googletagmanager.com ssl.gstatic.com www.gstatic.com data: *; style-src 'self' 'unsafe-inline' fonts.googleapis.comi tagmanager.google.com *.mailchimp.com; frame-src 'self' www.slideshare.net www.youtube.com www.googletagmanager.com"
         ></meta>
         {props.headComponents}
       </head>
